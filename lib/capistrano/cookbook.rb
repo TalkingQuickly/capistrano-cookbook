4284497a18c662545bf3f1920dcd94b4b3030197
Ben Dixon <ben@talkingquickly.co.uk>
2014-09-24 17:34:55 +0100

    don't include compile assets locally by default

--- lib/capistrano/cookbook.rb
+++ lib/capistrano/cookbook.rb
@@ -3,7 +3,6 @@ require "capistrano/cookbook/version"
 module Capistrano
   module Cookbook
     require 'capistrano/cookbook/check_revision'
-    require 'capistrano/cookbook/compile_assets_locally'
     require 'capistrano/cookbook/logs'
     require 'capistrano/cookbook/monit'
     require 'capistrano/cookbook/nginx'
