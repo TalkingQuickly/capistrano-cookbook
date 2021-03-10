4aa80d57bf8f5a6c04854098845970f189758795
Ben <ben@talkingquickly.co.uk>
2021-03-10 05:10:19 +0000

    refactors: all local config files to use a rails generator

--- lib/capistrano/cookbook/helpers/setup_config_values.rb
+++ lib/capistrano/cookbook/helpers/setup_config_values.rb
@@ -13,10 +13,6 @@ module Capistrano
         fetch(:config_files) || config_files_defaults
       end
 
-      def local_config_files
-        fetch(:local_config_files) || local_config_files_defaults
-      end
-
       private
 
       def symlinks_defaults
@@ -38,15 +34,6 @@ module Capistrano
           database.example.yml
           log_rotation
           secrets.yml
-        )
-      end
-
-      def local_config_files_defaults
-        %w(
-          nginx_conf.erb
-          puma_monit.conf.erb
-          puma.rb.erb
-          puma.service.erb
         )
       end
     end
---
+++ lib/generators/capistrano/reliably_deploying_rails/bootstrap_generator.rb
@@ -0,0 +1,41 @@
+require 'fileutils'
+
+module Capistrano
+  module ReliablyDeployingRails
+    module Generators
+      class BootstrapGenerator < Rails::Generators::Base
+        source_root File.expand_path('../templates', __FILE__)
+        desc "Bootstrap everything required to deploy with capistrano to a server configured with: https://github.com/TalkingQuickly/rails-server-template"
+
+        def create_capfile
+          template "Capfile.erb", 'Capfile'
+        end
+
+        def create_deployment_configuration
+          FileUtils.mkdir_p 'config/deploy'
+          template "deploy.rb.erb", 'config/deploy.rb'
+          template "production.rb.erb", 'config/deploy/production.rb'
+          template "staging.rb.erb", 'config/deploy/staging.rb'
+        end
+
+        def create_local_config_templates
+          FileUtils.mkdir_p 'config/deploy/templates'
+          base_path = File.expand_path('../templates', __FILE__)
+
+          %w(
+            nginx_conf.erb
+            puma_monit.conf.erb
+            puma.rb.erb
+            puma.service.erb
+          ).each do |file|
+            unless File.file?(file)
+              copy_file(File.join(base_path, file), "config/deploy/templates/#{file}")
+            end
+          end
+        end
+
+        private
+      end
+    end
+  end
+end
